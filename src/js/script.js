7748908ffc38d6816d6f2ec5810d4a5c085bba8b
unixnexo <unixnexo@gmail.com>
2024-07-11 16:31:06 +0330

    dev

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+
+
+
+///// test
+const searchInput = document.querySelector('input[type=search]');
+const searchIcon = document.getElementById('searchIcon');
+
+searchInput.addEventListener('focus', () => {
+    searchInput.classList.remove('pl-5');
+    searchInput.classList.add('pl-12');
+    searchIcon.classList.remove('hidden');
+    searchIcon.classList.add('flex');
+});
+
+searchInput.addEventListener('blur', () => {
+    searchInput.classList.add('pl-5');
+    searchInput.classList.remove('pl-12');
+    searchIcon.classList.add('hidden');
+    searchIcon.classList.remove('flex');
+});
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        "youtube-main-dark": "#0F0F0F",
+        "youtube-white-ish": "#C9C9C9",
+        "youtube-hover": "#272727",
+        "youtube-second-dark": "#222222",
+        "youtube-border": "#303030",
+        "youtube-blue-link": "#1C62B9",
+        "youtube-active": "#3D3D3D",
+        "youtube-tooltip": "#5B5B5B",
+      }
+    },
+  },
+  plugins: [],
+}
+
